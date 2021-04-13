d79b6f6bf38135c3b55fe8ef34cc806da1553db0
VihaanBansal <68996740+VihaanBansal@users.noreply.github.com>
2021-04-13 16:49:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+menu_list_array = ["Chicken Tandoori Pizza",
+"Veg Supreme Pizza",
+"Deluxe Veggie Pizza",
+"Paneer Tikka Pizza",
+"Veg Extravaganza Pizza"
+]; 
+
+function getMenu(){
+    document.getElementById("display_menu").innerHTML = menu_list_array ; 
+}
